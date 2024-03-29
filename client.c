b6cb4de4f7609f23af857f6ddde75eab0bdd0a42
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2024-03-29 16:22:00 -0400

    add validation on client side

--- client.c
+++ client.c
@@ -7,8 +7,27 @@
 #include<netinet/in.h>
 #include<netdb.h>
 
+void processCommand(int sockfd, char *buffer) {
+    int n;
+    n = write(sockfd, buffer, strlen(buffer));
+
+    if(n<0){
+        printf("Error on writing\n");
+    }
+
+    bzero(buffer, 1024);
+
+    n = read(sockfd, buffer, 1024);
+
+    if(n<0){
+        printf("Error on reading\n");
+    }
+
+    printf("%s\n",buffer);
+}
+
 int main(int argc, char *argv[]) {
-    int sockfd, portno, n;
+    int sockfd, portno;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
@@ -41,35 +60,32 @@ int main(int argc, char *argv[]) {
         fgets(buffer, 1024, stdin);
 
         if(strncmp(buffer,"quitc",strlen("quitc"))==0){
-            n = write(sockfd, buffer, strlen(buffer));
+            int n = write(sockfd, buffer, strlen(buffer));
             if(n<0){
                 printf("Error on writing\n");
             }
             printf("Closing connection on client side\n");
             break;
+        }else if(strncmp(buffer,"dirlist -a",strlen("dirlist -a"))==0){
+            processCommand(sockfd, buffer);
+        }else if(strncmp(buffer,"dirlist -t",strlen("dirlist -t"))==0){
+            processCommand(sockfd, buffer);           
+        }else if(strncmp(buffer,"w24fn",strlen("w24fn"))==0){
+            processCommand(sockfd, buffer);
+        }else if(strncmp(buffer,"w24fz",strlen("w24fz"))==0){
+            processCommand(sockfd, buffer);
+        }else if(strncmp(buffer,"w24ft",strlen("w24ft"))==0){
+            processCommand(sockfd, buffer);
+        }else if(strncmp(buffer,"w24fdb",strlen("w24fdb"))==0){
+            processCommand(sockfd, buffer);
+        }else if(strncmp(buffer,"w24fda",strlen("w24fda"))==0){
+            processCommand(sockfd, buffer);
+        }else if(strncmp(buffer,"hi",strlen("hi"))==0){
+            processCommand(sockfd,buffer);
         }
-
-        n = write(sockfd, buffer, strlen(buffer));
-
-        if(n<0){
-            printf("Error on writing\n");
+        else{
+            printf("Invalid command, please try again\n");
         }
-
-        bzero(buffer, 1024);
-
-        n = read(sockfd, buffer, 1024);
-
-        if(n<0){
-            printf("Error on reading\n");
-        }
-
-        printf("%s\n",buffer);
-
-        // int i=strncmp("bye",buffer,3);
-
-        // if(i==0) {
-        //     break;
-        // }
     }
     close(sockfd);
     return 0;
